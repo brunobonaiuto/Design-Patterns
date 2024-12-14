4bee8d52a1b480cf365fd0c66b79b79b243dc2ef
bbbolivar <bonaiuto@esteco.com>
2024-12-14 18:08:11 +0100

    builder example

---
+++ Builder/exampleBuilder/src/builder/Builder.java
@@ -0,0 +1,19 @@
+package builder;
+
+import components.Engine;
+import components.GPSNavigator;
+import components.Transmission;
+import components.TripComputer;
+import product.CarType;
+
+/**
+ * Builder interface defines all possible ways to configure a product.
+ */
+public interface Builder {
+    void setCarType(CarType type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+}
